2dffc5d32bcacb868d990e7b762dcde09b8fed1e
Runali Painaik <166627713+runali1fin@users.noreply.github.com>
2024-04-10 20:03:49 +0530

    feat: run 2-3 tests

--- tests/Web Automation/EngineeringChecklist.spec.js
+++ tests/Web Automation/EngineeringChecklist.spec.js
@@ -29,63 +29,63 @@ test.describe("QA Automation", () => {
             await homePage.closeDownloadScreen.click();
         });
 
-        test('Click on cashflow planning and assert response', async ({ page }) => {
-            await commonFunctions.scrollPage(page, 1);
-            await homePage.cashFlowPlanning.click();
-            await expect(homePage.cashFlowContent).toContainText("Cashflow Planning");
-        });
+        // test('Click on cashflow planning and assert response', async ({ page }) => {
+        //     await commonFunctions.scrollPage(page, 1);
+        //     await homePage.cashFlowPlanning.click();
+        //     await expect(homePage.cashFlowContent).toContainText("Cashflow Planning");
+        // });
 
-        test('Click on liability planning and assert response', async ({ page }) => {
-            await commonFunctions.scrollPage(page, 1);
-            await homePage.liabilityPlanning.click();
-            await expect(homePage.liabilityContent).toContainText("Liability Planning");
-        });
+        // test('Click on liability planning and assert response', async ({ page }) => {
+        //     await commonFunctions.scrollPage(page, 1);
+        //     await homePage.liabilityPlanning.click();
+        //     await expect(homePage.liabilityContent).toContainText("Liability Planning");
+        // });
 
-        test('Click on tax planning and assert response', async ({ page }) => {
-            await commonFunctions.scrollPage(page, 1);
-            await homePage.taxPlanning.click();
-            await expect(homePage.taxContent).toContainText("Tax Planning");
-        });
+        // test('Click on tax planning and assert response', async ({ page }) => {
+        //     await commonFunctions.scrollPage(page, 1);
+        //     await homePage.taxPlanning.click();
+        //     await expect(homePage.taxContent).toContainText("Tax Planning");
+        // });
 
-        test('Click on insurance planning and assert response', async ({ page }) => {
-            await commonFunctions.scrollPage(page, 1);
-            await homePage.insurancePlanning.click();
-            await expect(homePage.insuranceContent).toContainText("Insurance Planning");
-        });
+        // test('Click on insurance planning and assert response', async ({ page }) => {
+        //     await commonFunctions.scrollPage(page, 1);
+        //     await homePage.insurancePlanning.click();
+        //     await expect(homePage.insuranceContent).toContainText("Insurance Planning");
+        // });
 
-        test('Click on investment planning and assert response', async ({ page }) => {
-            await commonFunctions.scrollPage(page, 1)
-            await homePage.investmentPlanning.click();
-            await expect(homePage.investmentContent).toContainText("Investment Planning");
-        });
+        // test('Click on investment planning and assert response', async ({ page }) => {
+        //     await commonFunctions.scrollPage(page, 1)
+        //     await homePage.investmentPlanning.click();
+        //     await expect(homePage.investmentContent).toContainText("Investment Planning");
+        // });
 
-        test('Click on will planning and assert response', async ({ page }) => {
-            await commonFunctions.scrollPage(page, 1)
-            await homePage.willPlanning.click();
-            await expect(homePage.willContent).toContainText("Will & Estate Planning");
-        });
+        // test('Click on will planning and assert response', async ({ page }) => {
+        //     await commonFunctions.scrollPage(page, 1)
+        //     await homePage.willPlanning.click();
+        //     await expect(homePage.willContent).toContainText("Will & Estate Planning");
+        // });
 
-        test('Click on download the app in How it Works ? section', async ({ page }) => {
-            await commonFunctions.scrollPage(page, 2);
-            await homePage.downloadAppButtonTwo.click();
-            await expect(homePage.downloadTheAppContent).toContainText("Download 1 Finance");
-            await homePage.closeDownloadScreen.click();
-        });
+        // test('Click on download the app in How it Works ? section', async ({ page }) => {
+        //     await commonFunctions.scrollPage(page, 2);
+        //     await homePage.downloadAppButtonTwo.click();
+        //     await expect(homePage.downloadTheAppContent).toContainText("Download 1 Finance");
+        //     await homePage.closeDownloadScreen.click();
+        // });
 
-        test('Click on download the app of FWP section', async ({ page }) => {
-            await commonFunctions.scrollPage(page, 3);
-            await homePage.downloadAppButtonThree.click();
-            await expect(homePage.downloadTheAppContent).toContainText("Download 1 Finance");
-            await homePage.closeDownloadScreen.click();
-        });
+        // test('Click on download the app of FWP section', async ({ page }) => {
+        //     await commonFunctions.scrollPage(page, 3);
+        //     await homePage.downloadAppButtonThree.click();
+        //     await expect(homePage.downloadTheAppContent).toContainText("Download 1 Finance");
+        //     await homePage.closeDownloadScreen.click();
+        // });
 
-        test('To check expansion and collapse tile in FAQ section', async ({ page }) => {
-            await homePage.faqFirstTile.click();
-            await homePage.faqSecondTile.click();
-            await homePage.faqThirdTile.click();
-            await homePage.faqFourthTile.click();
-            await homePage.faqFifthTile.click();
-        });
+        // test('To check expansion and collapse tile in FAQ section', async ({ page }) => {
+        //     await homePage.faqFirstTile.click();
+        //     await homePage.faqSecondTile.click();
+        //     await homePage.faqThirdTile.click();
+        //     await homePage.faqFourthTile.click();
+        //     await homePage.faqFifthTile.click();
+        // });
 
     // })
 
